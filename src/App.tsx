0ab028053b4b9ee7f9773268b705859219fafd9c
ungs <yachae85@gmail.com>
2022-07-21 22:31:28 +0900

    initial settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Reset } from 'styled-reset';
+import GlobalStyle from './GlobalStyle';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <React.Fragment>
+    <Reset />
+    <GlobalStyle />
+    <div>hello world!!</div>
+  </React.Fragment>
+);
 
 export default App;
